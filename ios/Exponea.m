dc1c3974166dcb247889247a7618738cdfa6a86a
Juraj Dudak <juraj.dudak@panaxeo.com>
2020-06-19 16:08:22 +0200

    feat/Add iOS implementation for tracking

--- ios/Exponea.m
+++ ios/Exponea.m
@@ -18,4 +18,10 @@ RCT_EXTERN_METHOD(setFlushPeriod:(NSNumber * _Nonnull)flushPeriod resolve:(RCTPr
 RCT_EXTERN_METHOD(getLogLevel:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(setLogLevel:(NSString *)logLevel resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 
+RCT_EXTERN_METHOD(identifyCustomer:(NSDictionary *)ids properties:(NSDictionary *)properties resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(flushData:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(trackEvent:(NSString *)eventType properties:(NSDictionary *)properties timestamp:(NSDictionary *)timestamp resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(trackSessionStart:(NSDictionary *)timestamp resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(trackSessionEnd:(NSDictionary *)timestamp resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
 @end
--- ios/Exponea.swift
+++ ios/Exponea.swift
@@ -158,4 +158,88 @@ class Exponea: NSObject {
         }
     }
 
+    @objc(identifyCustomer:properties:resolve:reject:)
+    func identifyCustomer(
+        customerIds: NSDictionary,
+        properties: NSDictionary,
+        resolve: RCTPromiseResolveBlock,
+        reject: RCTPromiseRejectBlock
+    ) {
+        guard ExponeaSDK.Exponea.shared.isConfigured else {
+            rejectPromise(reject, error: ExponeaError.notConfigured)
+            return
+        }
+        do {
+            ExponeaSDK.Exponea.shared.identifyCustomer(
+                customerIds: try JsonDataParser.parse(dictionary: customerIds),
+                properties: try JsonDataParser.parse(dictionary: properties),
+                timestamp: nil
+            )
+            resolve(nil)
+        } catch {
+            rejectPromise(reject, error: error)
+        }
+    }
+
+    @objc(flushData:reject:)
+    func flushData(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+        guard ExponeaSDK.Exponea.shared.isConfigured else {
+            rejectPromise(reject, error: ExponeaError.notConfigured)
+            return
+        }
+        ExponeaSDK.Exponea.shared.flushData()
+        resolve(nil)
+    }
+
+    @objc(trackEvent:properties:timestamp:resolve:reject:)
+    func trackEvent(
+        eventType: String,
+        properties: NSDictionary,
+        timestamp: NSDictionary,
+        resolve: RCTPromiseResolveBlock,
+        reject: RCTPromiseRejectBlock
+    ) {
+        guard ExponeaSDK.Exponea.shared.isConfigured else {
+            rejectPromise(reject, error: ExponeaError.notConfigured)
+            return
+        }
+        do {
+            ExponeaSDK.Exponea.shared.trackEvent(
+                properties: try JsonDataParser.parse(dictionary: properties),
+                timestamp: try timestamp.getOptionalSafely(property: "timestamp"),
+                eventType: eventType
+            )
+            resolve(nil)
+        } catch {
+            rejectPromise(reject, error: error)
+        }
+    }
+
+    @objc(trackSessionStart:resolve:reject:)
+    func trackSessionStart(timestamp: NSDictionary, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+        guard ExponeaSDK.Exponea.shared.isConfigured else {
+            rejectPromise(reject, error: ExponeaError.notConfigured)
+            return
+        }
+        guard timestamp.object(forKey: "timestamp") == nil else {
+            rejectPromise(reject, error: ExponeaError.notAvailableForPlatform(name: "Setting session start timestamp"))
+            return
+        }
+        ExponeaSDK.Exponea.shared.trackSessionStart()
+        resolve(nil)
+    }
+
+    @objc(trackSessionEnd:resolve:reject:)
+    func trackSessionEnd(timestamp: NSDictionary, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+        guard ExponeaSDK.Exponea.shared.isConfigured else {
+            rejectPromise(reject, error: ExponeaError.notConfigured)
+            return
+        }
+        guard timestamp.object(forKey: "timestamp") == nil else {
+            rejectPromise(reject, error: ExponeaError.notAvailableForPlatform(name: "Setting session end timestamp"))
+            return
+        }
+        ExponeaSDK.Exponea.shared.trackSessionEnd()
+        resolve(nil)
+    }
 }
